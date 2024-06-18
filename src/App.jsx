f01c7d469d3d09f250a7b27ae2a016b8059ee3ff
AngelUrrego <angelurrego2010@gmail.com>
2024-06-17 21:58:03 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './styles/App.css'
 import './styles/navbar.css'
 import Home from './pages/Home'
@@ -7,17 +7,29 @@ import Episodes from './pages/Episodes'
 import serie from './assets/serie.png'
 import NotFound from './pages/NotFound'
 import Location from './pages/Location'
+import { useState } from 'react'
 
 function App() {
+  const [MenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!MenuOpen);
+  }
+
   return (
     <Router>
-        <div className='nav'>
-              <img  src={serie} alt="" />  
-              <Link className="link" to="/">Inicio</Link>
-              <Link className="link" to='/characters'>Personajes</Link>
-              <Link className="linkE" to='/episodes'>Episodios</Link>
-              <Link className="linkE" to='/location'>Locaciones</Link>
+      <div className='nav'>
+        <img src={serie} alt="Serie Logo" />
+        <div className={`links ${MenuOpen ? 'open' : ''}`}>
+          <Link className="link" to="/" onClick={() => setMenuOpen(false)}>Inicio</Link>
+          <Link className="link" to='/characters' onClick={() => setMenuOpen(false)}>Personajes</Link>
+          <Link className="linkE" to='/episodes' onClick={() => setMenuOpen(false)}>Episodios</Link>
+          <Link className="linkE" to='/location' onClick={() => setMenuOpen(false)}>Locaciones</Link>
         </div>
+        <button className='hamburger' onClick={toggleMenu}>
+          &#9776;
+        </button>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/characters" element={<Characters />} />
@@ -26,7 +38,6 @@ function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
-    
   )
 }
 
